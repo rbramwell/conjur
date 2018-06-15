84d8fb656e852aef6677ac5b2beac0fc196e3316
Rafał Rzepecki <divided.mind@gmail.com>
2018-06-15 09:46:22 -0400

    Authn audit events

---
+++ app/models/audit/event/authn.rb
@@ -0,0 +1,53 @@
+require 'ostruct'
+
+class Audit::Event::Authn < OpenStruct
+  def emit_success
+    self.success = true
+    Audit.info message, 'authn', structured_data
+  end
+
+  def emit_failure error_message
+    self.error_message = error_message
+    self.success = false
+    Audit.warn message, 'authn', structured_data
+  end
+
+  def message
+    if success?
+      SUCCESS_TEMPLATE % [role_id, authenticator_name, service_id]
+    else
+      FAILURE_TEMPLATE % [role_id, authenticator_name, service_id, error_message]
+    end
+  end
+
+  SUCCESS_TEMPLATE = "%s successfully authenticated with authenticator %s service %s".freeze
+  FAILURE_TEMPLATE = "%s failed to authenticate with authenticator %s service %s: %s".freeze
+
+  def role_id
+    role.id
+  end
+
+  def service_id
+    service.try :id
+  end
+
+  def success?
+    !!success
+  end
+
+  SDID = ::Audit::SDID
+
+  def structured_data
+    {
+      SDID::SUBJECT => { role: role_id },
+      SDID::AUTH => {
+        authenticator: authenticator_name,
+        service: service_id
+      },
+      SDID::ACTION => {
+        operation: 'authenticate',
+        result: success?? 'success' : 'failure'
+      }
+    }
+  end
+end
